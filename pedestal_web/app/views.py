7cbdcaa4f75f6ea470b522319539c211170b0c47
ox <ox@ox.com>
2016-05-10 22:18:44 +0300

    portal

--- pedestal_web/app/views.py
+++ pedestal_web/app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
+from flask.ext.appbuilder import AppBuilder, expose, BaseView, has_access
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
 
@@ -16,6 +17,29 @@ from app import appbuilder, db
 
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
+
+class MyView(BaseView):
+
+    default_view = 'method1'
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # do something with param1
+        # and return it
+        return "hello"
+
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render it
+        param1 = 'Goodbye %s' % (param1)
+        return param1
+
+appbuilder.add_view(MyView, "Method2", category="My View")
+appbuilder.add_link("Method2", href="myview/method2/peter", category="My View")
+
 
 """
     Application wide 404 error handler
