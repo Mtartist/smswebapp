097622aded7a0899a2ccd31bda5e05c4bddc1dd2
ox <ox@ox.com>
2016-05-14 19:07:28 +0300

    pd

--- pedestal_web/app/views.py
+++ pedestal_web/app/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, flash
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import AppBuilder, expose, BaseView, has_access
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
+from flask_appbuilder import SimpleFormView
+from flask.ext.babelpkg import lazy_gettext as _
+from forms import TextAlertForm
 
 """
     Create your Views::
@@ -18,28 +21,51 @@ from app import appbuilder, db
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
 
-class MyView(BaseView):
 
-    default_view = 'method1'
+class Alert(BaseView):
 
-    @expose('/method1/')
+    default_view = 'text_alert'
+
+    @expose('/text_alert/')
     @has_access
-    def method1(self):
+    def text_alert(self):
         # do something with param1
         # and return it
-        return "hello"
+        message="Hello Peter"
+        self.update_redirect()
+        return self.render_template('text_alert.html',
+                           message=message)
 
-    @expose('/method2/<string:param1>')
+    @expose('/multimedia_alert/')
     @has_access
-    def method2(self, param1):
+    def multimedia_alert(self):
         # do something with param1
         # and render it
-        param1 = 'Goodbye %s' % (param1)
-        return param1
+        _user = "Peter"
+        message = 'Goodbye %s' % (_user)
+        self.update_redirect()
+        return self.render_template('text_alert.html',
+                           message=message)
 
-appbuilder.add_view(MyView, "Method2", category="My View")
-appbuilder.add_link("Method2", href="myview/method2/peter", category="My View")
+appbuilder.add_view(Alert, "Create Text Alert", category="Text Alerts")
+appbuilder.add_link("Create Multimedia Alert", href="alert/multimedia_alert/",
+     category="Content Alerts")
 
+
+class AlertFormView(SimpleFormView):
+    form = TextAlertForm
+    form_title = 'This is alerts form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.text_msg.data = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+appbuilder.add_view(AlertFormView, "Alert form View", icon="fa-group", label=_('Alert form View'),
+                     category="Alert Forms", category_icon="fa-cogs")
 
 """
     Application wide 404 error handler
@@ -49,5 +75,3 @@ def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
 
 db.create_all()
-
-
