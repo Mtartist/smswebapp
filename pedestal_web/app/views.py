9b97c593230621f9b8ba2403e2297817229f6916
ox <ox@ox.com>
2017-01-09 00:00:19 +0300

    pedestal portal

--- pedestal_web/app/views.py
+++ pedestal_web/app/views.py
@@ -3,7 +3,7 @@ from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import ModelView, CompactCRUDMixin
 from app import appbuilder, db
 from models import Service, Alert, ShortCode, Content, MediaFile, Artist,\
- Keyword, Client, ModelType
+ Keyword, Client, ModelType, Subscriber, Inbox, Outbox,SdpProduct,UserClient,Profile,ContentService,ArtistRevShare,BulkSender,BulkSenderClient,Client,ContactGroup,ContactGroup,ContactGroupProfile
 
 
 class KeywordModelView(ModelView):
@@ -76,6 +76,59 @@ class ModelTypeModelView(ModelView):
     list_columns = ['name', 'model', 'status', 'created']
 
 
+class SubscriberModelView(ModelView):
+    datamodel = SQLAInterface(Subscriber)
+    list_columns = ['profile_id', 'service_id', 'status','created', 'modified']
+
+
+class InboxModelView(ModelView):
+    datamodel = SQLAInterface(Inbox)
+    list_columns = ['message','msisdn','short_code_id','link_id','service_id','profile_id','created','modified']
+
+
+class OutboxModelView(ModelView):
+    datamodel = SQLAInterface(Outbox)
+    list_columns = ['alert_id', 'text', 'msisdn', 'content_id_hash', 'ref_number', 'dlr_status', 'processed', 'sends', 'time_sent', 'created', 'modified']
+
+class SdpProductModelView(ModelView):
+    datamodel = SQLAInterface(SdpProduct)
+    list_columns = ['service','product','shortcode_id','keyword_id','status','created','modified']
+
+class UserClientModelView(ModelView):
+    datamodel = SQLAInterface(UserClient)
+    list_columns = ['user_id','client_id']
+
+class ProfileModelView(ModelView):
+    datamodel = SQLAInterface(Profile)
+    list_columns = ['msisdn','network_id','created','modified']
+
+class ContentServiceModelView(ModelView):
+    datamodel = SQLAInterface(ContentService)
+    list_columns = ['content_id','service_id']
+
+class ArtistRevShareModelView(ModelView):
+    datamodel = SQLAInterface(ArtistRevShare)
+    list_columns = ['artist_id' ,'contract_id','rev_share','status','created','modified']
+
+class BulkSenderModelView(ModelView):
+    datamodel = SQLAInterface(BulkSender)
+    list_columns = ['name','sdp_service_id','sdp_access_code','status','created','modified']
+
+class BulkSenderClientModelView(ModelView):
+    datamodel = SQLAInterface(BulkSenderClient)
+    list_columns = ['bulksender_id','client_id']
+
+class ContactGroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    list_columns = ['name','client_id','status','created','modified']
+
+class ContactGroupProfileModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroupProfile)
+    list_columns = ['contactgroup_id','profile_id']
+
+
+
+
 """
     Application wide 404 error handler
 """
@@ -88,23 +141,58 @@ def page_not_found(e):
 
 db.create_all()
 appbuilder.add_view(AlertModelView, "Alerts", icon="fa-table",
-     category="Content Management",
+     category="Messaging",
                 category_icon="fa-envelope")
 appbuilder.add_view(ServiceModelView, "Services", icon="fa-table",
-     category="Content Management")
+     category="Settings")
 appbuilder.add_view(ClientModelView, "Clients", icon="fa-table",
      category="Content Management")
 appbuilder.add_view(ModelTypeModelView, "ModelTypes", icon="fa-table",
-     category="Content Management")
+     category="Settings")
 appbuilder.add_view(ShortcodeModelView, "Shortcode", icon="fa-table",
+     category="Settings")
+appbuilder.add_view(SubscriberModelView, "Subscriber", icon="fa-table",
      category="Content Management")
 appbuilder.add_view(ContentModelView, "Contents", icon="fa-folder-open-o",
-     category="Multimedia",
+     category="Content",
                 category_icon="fa-envelope")
 appbuilder.add_view_no_menu(MediaFilesModelView)
 appbuilder.add_view(ArtistModelView, "Artists", icon="fa-table",
-     category="Multimedia",
+     category="Content",
                 category_icon="fa-envelope")
 appbuilder.add_view(KeywordModelView, "Keywords", icon="fa-table",
-     category="Multimedia",
+     category="Settings",
+                category_icon="fa-envelope")
+appbuilder.add_view(InboxModelView, "Inbox", icon="fa-table",
+     category="Messaging",
+                category_icon="fa-envelope")
+appbuilder.add_view(OutboxModelView, "Outbox", icon="fa-table",
+     category="Messaging",
+                category_icon="fa-envelope")
+appbuilder.add_view(SdpProductModelView, "SdpProducts", icon="fa-table",
+     category="Settings",
+                category_icon="fa-envelope")
+appbuilder.add_view(UserClientModelView, "UserClientMap", icon="fa-table",
+     category="Settings",
+                category_icon="fa-envelope")
+appbuilder.add_view(ProfileModelView, "Profiles", icon="fa-table",
+     category="Content Management",
+                category_icon="fa-envelope")
+appbuilder.add_view(ContentServiceModelView, "ContentServiceMap", icon="fa-table",
+     category="Content",
+                category_icon="fa-envelope")
+appbuilder.add_view(ArtistRevShareModelView, "ArtistRevShares", icon="fa-table",
+     category="Content Management",
+                category_icon="fa-envelope")
+appbuilder.add_view(BulkSenderModelView, "BulkSenders", icon="fa-table",
+     category="Bulk SMS",
+                category_icon="fa-envelope")
+appbuilder.add_view(BulkSenderClientModelView, "BulkSenderClientMap", icon="fa-table",
+     category="Bulk SMS",
+                category_icon="fa-envelope")
+appbuilder.add_view(ContactGroupModelView, "ContactGroups", icon="fa-table",
+     category="Bulk SMS",
+                category_icon="fa-envelope")
+appbuilder.add_view(ContactGroupProfileModelView, "ContactGroupProfileMap", icon="fa-table",
+     category="Bulk SMS",
                 category_icon="fa-envelope")
